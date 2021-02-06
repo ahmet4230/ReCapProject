eec1a40c8446d62b4bf0705c222e4db9a76760ce
ahmet batman <ahmet_batman123@hotmail.com>
2021-02-06 03:49:26 +0300

    Güncelleme

--- Console/Program.cs
+++ Console/Program.cs
@@ -12,10 +12,12 @@ namespace ConsoleUI
             {
             CarManager carmanager = new CarManager(new EfCarDal());
             //carmanager.Add(new Car{ BrandId = 1, ColorId = 1, ModelYear = 2010, DailyPrice = 1000, Description = "OPEL" });
+            // hatalı kullanım carmanager.Add(new Car { BrandId = 2, ColorId = 2, ModelYear = 2011, DailyPrice = -1500, Description = "TO" });
             foreach (var car in carmanager.GetAll())
             {
                 Console.WriteLine("Araç ismi: "  + car.Description + "Aracın günlük fiyatı: " + car.DailyPrice);
             }
+            
                 
 
             }
