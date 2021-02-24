cac32ef30bb8982f48160d0e8d5b2826be3393f1
ahmet batman <ahmet_batman123@hotmail.com>
2021-02-24 17:11:04 +0300

    Autofac , FluentValidation , AOP desteği eklendi.

--- Core/CrossCuttingConcerns/Validation/ValidationTool.cs
+++ Core/CrossCuttingConcerns/Validation/ValidationTool.cs
@@ -7,6 +7,7 @@ namespace Core.CrossCuttingConcerns.Validation
 {
     public static class ValidationTool
     {
+
         public static void Validate(IValidator validator,object entity)
         {
             var context = new ValidationContext<Object>(entity);
